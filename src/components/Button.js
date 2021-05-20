e43d8873a5322baefc9dd5519b61d550bad5e6f9
Ata-92 <atakal92@gmail.com>
2021-05-20 14:42:01 +0300

    Add a ternary and onClick event

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
+import AddTask from './AddTask';
 
 const Button = () => {
   const [show, setShow] = useState(false);
 
   return (
     <div className="Button">
+      {show ? (
+        <div className="wrapper">
+          <button onClick={showClose}>Close Add Task Bar</button>
+          <AddTask />
+        </div>
+      ) : (
+        <button onClick={showClose}>Show Add Task Bar</button>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Button;
