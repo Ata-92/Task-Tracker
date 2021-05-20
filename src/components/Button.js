51d0a7799f45d0819d12fb55d4b0e27fb2cb0c29
Ata-92 <atakal92@gmail.com>
2021-05-20 14:32:47 +0300

    Add a state to Button.js

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Button = () => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className="Button">
+    </div>
+  )
+}
+
+export default Button;
