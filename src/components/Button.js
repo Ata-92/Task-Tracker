6ec72ad1e8e8f17906026d8fb99afeee19ec8977
Ata-92 <atakal92@gmail.com>
2021-05-24 01:12:30 +0300

    Define functions in Button.js and send to children

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,48 @@
 import React, { useState } from "react";
-import AddTask from './AddTask';
+import AddTask from "./AddTask";
+import Tasks from "./Tasks";
 
 const Button = () => {
   const [show, setShow] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task, dayTime) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    setTasks([...tasks, {id, task, dayTime }]);
+  };
+
+  const deleteTask = (id) => {
+    console.log(id);
+    setTasks(tasks.filter((task) => (task.id !== id)));
+  }
 
   const openClose = () => {
     setShow(!show);
-  }
+  };
 
   return (
     <div className="Button">
       {show ? (
         <div className="container">
-          <button className="btn btn-danger px-3 text-light d-block mx-auto closeButton" onClick={openClose}>Close Add Task Bar</button>
-          <AddTask />
+          <button
+            className="btn btn-danger px-3 text-light d-block mx-auto closeButton"
+            onClick={openClose}
+          >
+            Close Add Task Bar
+          </button>
+          <AddTask addTask={addTask} />
+          <Tasks tasks={tasks} deleteTask={deleteTask}/>
         </div>
       ) : (
-        <button className="btn text-light px-3 d-block mx-auto showButton" onClick={openClose}>Show Add Task Bar</button>
+        <div className="container">
+          <button
+            className="btn text-light px-3 d-block mx-auto showButton"
+            onClick={openClose}
+          >
+            Show Add Task Bar
+          </button>
+          <Tasks tasks={tasks} deleteTask={deleteTask}/>
+        </div>
       )}
     </div>
   );
