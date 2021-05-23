903807145cd3318e2de7e8bb7e1e2375f3187270
Ata-92 <atakal92@gmail.com>
2021-05-24 01:09:28 +0300

    Add a function property to AddTask.js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,13 +1,11 @@
 import { useState } from "react";
-import Task from './Task';
 
-const AddTask = () => {
+const AddTask = ({ addTask }) => {
   const [task, setTask] = useState("");
   const [dayTime, setDayTime] = useState("");
-  const [add, setAdd] = useState(false);
 
   const saveTask = () => {
-    task && setAdd(true);
+    addTask(task, dayTime);
     setTask("");
     setDayTime("");
   }
@@ -16,20 +14,44 @@ const AddTask = () => {
     <div className="AddTask">
       <form className="form" action="/" method="get">
         <div className="container">
-          <label htmlFor="task" className="form-label">Task</label>
-          <input id="task" className="form-control" type="text" value={task} onChange={(e) => setTask(e.target.value)} placeholder="AddTask" required/>
+          <label htmlFor="task" className="form-label">
+            Task
+          </label>
+          <input
+            id="task"
+            className="form-control"
+            type="text"
+            value={task}
+            onChange={e => setTask(e.target.value)}
+            placeholder="AddTask"
+            required
+          />
         </div>
         <div className="container">
-          <label htmlFor="dt" className="form-label">Day & Time</label>
-          <input id="dt" className="form-control" type="text" value={dayTime} onChange={(e) => setDayTime(e.target.value)} placeholder="Add Day & Time"/>
+          <label htmlFor="dt" className="form-label">
+            Day & Time
+          </label>
+          <input
+            id="dt"
+            className="form-control"
+            type="text"
+            value={dayTime}
+            onChange={e => setDayTime(e.target.value)}
+            placeholder="Add Day & Time"
+          />
         </div>
         <div className="container">
-          <button className="btn text-light w-100 saveButton" type="button" onClick={saveTask}>Save Task</button>
+          <button
+            className="btn text-light w-100 saveButton"
+            type="button"
+            onClick={saveTask}
+          >
+            Save Task
+          </button>
         </div>
       </form>
-      {add && <Task task={task} dayTime={dayTime}/>}
     </div>
-  )
-}
+  );
+};
 
 export default AddTask;
